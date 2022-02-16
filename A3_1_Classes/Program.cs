081e2c52e6cf4e3c86fd77075e44ea26fc4faa91
GRADEN BISHOP <GRADEN.BISHOP2995@OMAIL.OTECH.EDU>
2022-02-16 14:38:56 -0700

    first commit

---
+++ A3_1_Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A3_1_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
